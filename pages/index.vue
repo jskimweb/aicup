4754ee33cafefb48165e7fd2945f781e0db9c072
jskimweb <jskimweb@gmail.com>
2023-12-10 05:02:47 +0900

    edit position of main title

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <h1 class="text-center">아이컵, 아이돌 이상형 월드컵</h1>
+  <div class="h-screen flex flex-col justify-center items-center">
+    <h1 class="text-center">
+      <span class="text-center">아이돌 이상형 월드컵</span>
+      <br />
+      <span>아이컵</span>
+    </h1>
   </div>
 </template>
